e30d674ac56cd13162b9bca31d5a6e589ead31a1
Alexandre Awadallak <alexandre.awadallak@gmail.com>
2022-06-05 16:23:02 -0300

    truffle init

--- contracts/IStorage.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >= 0.4.24 <0.9.0;
-
-// Define a SimpleStora interface
-// This is a contract that has only view functions
-// It can be used to define a contract that has only view functions
